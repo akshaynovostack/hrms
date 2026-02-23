72dd00d1f4c397da307bab41c351941ec753a33a
Akshay Sharma <akshay.sharma@novostack.com>
2026-02-23 14:23:45 +0000

    Server Changes for HRMS

--- hrms/hooks.py
+++ hrms/hooks.py
@@ -1,19 +1,19 @@
-app_name = "hrms"
-app_title = "Frappe HR"
-app_publisher = "Frappe Technologies Pvt. Ltd."
+app_name = "NovoStack HRMS"
+app_title = "NovoStack HR"
+app_publisher = "NovoStack Pvt. Ltd."
 app_description = "Modern HR and Payroll Software"
 app_email = "contact@frappe.io"
 app_license = "GNU General Public License (v3)"
 required_apps = ["frappe/erpnext"]
 source_link = "http://github.com/frappe/hrms"
-app_logo_url = "/assets/hrms/images/frappe-hr-logo.svg"
+app_logo_url = "/assets/hrms/images/novostack-hr-logo.svg"
 app_home = "/desk/people"
 
 add_to_apps_screen = [
 	{
-		"name": "hrms",
-		"logo": "/assets/hrms/images/frappe-hr-logo.svg",
-		"title": "Frappe HR",
+		"name": "NovoStack",
+		"logo": "/assets/hrms/images/novostack-hr-logo.svg",
+		"title": "NovoStack HR",
 		"route": "/desk/people",
 		"has_permission": "hrms.hr.utils.check_app_permission",
 	}
